f4c057d458af3266dfc010d3f3a7ea74e3925091
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-27 15:59:34 +0530

    Added: Support for notification each second ( onway )

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use tray_item::{TrayItem, IconSource};
+extern crate notify_rust;
+extern crate gtk;
+
+use notify_rust::Notification;
+use std::thread;
+use std::time::Duration;
+
+use tray_item::{IconSource, TrayItem};
 
 fn main() {
     gtk::init().unwrap();
@@ -15,5 +22,34 @@ fn main() {
         gtk::main_quit();
     }).unwrap();
 
+    // Spawn a thread to send notifications
+    thread::spawn(|| {
+        send_notifications();
+    });
+
     gtk::main();
+}
+
+fn send_notifications() {
+    loop {
+        // Send notification
+        send_notification("Hello from Rust!");
+
+        // Sleep for one second
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn send_notification(message: &str) {
+    // Create a new notification
+    let notification = Notification::new()
+        .summary("Rust Notification")
+        .body(message)
+        .icon("dialog-information")
+        .finalize(); // Use finalize to construct the Notification object
+
+    // Check if the notification is supported
+    if let Err(error) = notification.show() {
+        eprintln!("Error showing notification: {:?}", error);
+    }
 }
